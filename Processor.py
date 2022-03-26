eea1a5fe10a723e01853296ef9a8ea57c17e0acc
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-03-26 14:26:29 -0400

    [FEAT] add Processor mock

--- Processor.py
+++ Processor.py
@@ -13,3 +13,20 @@ class DriverProcessor(Processor):
 
   def process(self, listOfMessages: list[int]) -> None:
       print(listOfMessages)
+
+class MockProcessor(Processor):
+  '''Mock Processor used for Testing'''
+  def __init__(self, expectedListOfMessages: list[list[int]]) -> None:
+    self.expectedListOfMessages = expectedListOfMessages
+    self.timesCalled = 0
+    super().__init__()
+
+  
+  def process(self, listOfMessages: list[int]) -> None:
+    '''Mocks implementation of Processor and checks for correct implementation'''
+
+    assert self.timesCalled < len(self.expectedListOfMessages), "Process messages was called too many times"
+    expectedList = self.expectedListOfMessages[self.timesCalled]
+    self.timesCalled += 1
+
+    assert expectedList == listOfMessages, "list of messages processed is not correct"
