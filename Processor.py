00c9bcb3e209090276c902863172ae53b678ecfe
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-03-26 14:47:43 -0400

    [CHORE] update naming

--- Processor.py
+++ Processor.py
@@ -4,29 +4,29 @@ class Processor(ABC):
   '''Abstract class for a Processor'''
 
   @abstractmethod
-  def process(self, listOfMessages: list[int]) -> None:
+  def process(self, list_of_messages: list[int]) -> None:
     pass
 
 
 class DriverProcessor(Processor):
   '''Driver code for the Processor'''
 
-  def process(self, listOfMessages: list[int]) -> None:
-      print(listOfMessages)
+  def process(self, list_of_messages: list[int]) -> None:
+      print(list_of_messages)
 
 class MockProcessor(Processor):
   '''Mock Processor used for Testing'''
-  def __init__(self, expectedListOfMessages: list[list[int]]) -> None:
-    self.expectedListOfMessages = expectedListOfMessages
-    self.timesCalled = 0
+  def __init__(self, expected_list_of_messages: list[list[int]]) -> None:
+    self.expected_list_of_messages = expected_list_of_messages
+    self.times_called = 0
     super().__init__()
 
   
-  def process(self, listOfMessages: list[int]) -> None:
+  def process(self, list_of_messages: list[int]) -> None:
     '''Mocks implementation of Processor and checks for correct implementation'''
 
-    assert self.timesCalled < len(self.expectedListOfMessages), "Process messages was called too many times"
-    expectedList = self.expectedListOfMessages[self.timesCalled]
-    self.timesCalled += 1
+    assert self.times_called < len(self.expected_list_of_messages), "Process messages was called too many times"
+    expected_list = self.expected_list_of_messages[self.times_called]
+    self.times_called += 1
 
-    assert expectedList == listOfMessages, "list of messages processed is not correct"
+    assert expected_list == list_of_messages, "list of messages processed is not correct"
