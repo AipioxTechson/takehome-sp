1fa721919f2871fdaae6eaa9d5d9f59e97ead44b
Ritvik Bhardwaj <ritvik.bhardwaj@outlook.com>
2022-03-26 14:35:24 -0400

    [CHORE] update naming

--- Listener.py
+++ Listener.py
@@ -17,36 +17,38 @@ class Listener(ABC):
     pass
 
 
-class collatingListener(Listener):
+class CollatingListener(Listener):
   '''
   This class represents the implementation for a Listener
   '''
-  def __init__(self, processor: Processor, messagesObtained: set[int], messageLock: Lock):
+  def __init__(self, processor: Processor, messages_obtained: set[int], message_lock: Lock):
     super().__init__(processor)
-    self.curKey = 0
-    self.messagesObtained = messagesObtained
-    self.lock = messageLock
+    self.cur_key = 0
+    self.messages_obtained = messages_obtained
+    self.lock = message_lock
 
   def add_to_message_table(self, listOfMessages: list[int]) -> None:
     '''Adds messages to the saved list, assumes lock is held'''
+
     for message in listOfMessages:
-      self.messagesObtained.add(message)
+      self.messages_obtained.add(message)
 
 
   def extract_steam_of_messages(self) -> list[int]:
     '''Extracts the current stream of messages, assumes lock is held'''
+
     messages = []
-    while self.curKey in self.messagesObtained:
-      messages.append(self.curKey)
-      self.messagesObtained.discard(self.curKey)
-      self.curKey += 1
+    while self.cur_key in self.messages_obtained:
+      messages.append(self.cur_key)
+      self.messages_obtained.discard(self.cur_key)
+      self.cur_key += 1
     return messages
 
   def process(self, messages: list[int]) -> None:
       '''Recieves a list of messages and invokes the processor when a stream of messages comes in'''
       self.lock.acquire()
       self.add_to_message_table(messages)
-      if self.curKey in self.messagesObtained:
+      if self.cur_key in self.messages_obtained:
         messages = self.extract_steam_of_messages()
         self.processor.process(messages)
       self.lock.release()
@@ -54,9 +56,9 @@ class collatingListener(Listener):
 
 if __name__ == "__main__":
   processor = DriverProcessor()
-  messagesObtained = set()
+  messages_obtained = set()
   lock = Lock()
-  listener = collatingListener(processor, messagesObtained, lock)
+  listener = CollatingListener(processor, messages_obtained, lock)
 
   # Example code
   listener.process([1,4])
